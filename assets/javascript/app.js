f4eacf9b4fd14a72cffc53c9b39a6516422a854a
Blake Neely <blakeneely21@gmail.com>
2019-08-27 17:30:11 -0500

    added ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,39 @@
 $(document).ready(function(){
-    var options = ["ryan gosling", "jack black", "michael j fox", "john stamos", "steve carrell"];
+    var options = ["ryan gosling", "emma stone", "michael j fox", "john stamos", "steve carrell"];
     var userInput;
+    var actor;
+
+    // function to get gifs from giphy
+    function renderGifs(){
+        var actor = $(this).attr("data-name");
+        console.log(this)
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q" + actor + "&api_key=08uHsmK4UzYd8KoIXuWdAWPu4gZtNIB5&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+            var results = response.data;
+            for (var i = 0; i < results.length; i++){
+                var card = $("<div>");
+                card.addClass("card");
+                card.css("width","18rem");
+                var image = $("<img>");
+                image.attr("src",results[i].images.fixed_height.url)
+                image.addClass("card-img-top");
+                card.append(image);
+                $(".gif-container").append(card);
+            }
+        });
+    };
 
     // function to make buttons from array and append to html
     function renderButtons() {
         for (var i = 0; i < options.length; i++) {
             var btn = $("<button>");
+            btn.addClass("actor");
+            btn.attr("data-name", options[i]);
             var actorBtn = btn.text(options[i]);
             $(".btn-container").append(actorBtn);
         }
@@ -30,7 +58,9 @@ $(document).ready(function(){
 
     // function on click 
 
-
+    $(document).on("click", ".actor", function(){
+        renderGifs();
+    });
 
     renderButtons();
 });
