b570f54efd84231a7ccf562c3f1b3f4b66cc8a20
Blake Neely <blakeneely21@gmail.com>
2019-08-27 18:35:10 -0500

    pushed images to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,34 +5,34 @@ $(document).ready(function(){
 
     // function to get gifs from giphy
     function renderGifs(){
-        var actor = $(this).attr("data-name");
-        console.log(this)
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q" + actor + "&api_key=08uHsmK4UzYd8KoIXuWdAWPu4gZtNIB5&limit=10";
+        // var actor = $(this).attr("data-name");
+        // console.log(this)
+        // var queryURL = "https://api.giphy.com/v1/gifs/search?q" + actor + "&api_key=08uHsmK4UzYd8KoIXuWdAWPu4gZtNIB5&limit=10";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response){
-            console.log(response);
-            var results = response.data;
-            for (var i = 0; i < results.length; i++){
-                var card = $("<div>");
-                card.addClass("card");
-                card.css("width","18rem");
-                var image = $("<img>");
-                image.attr("src",results[i].images.fixed_height.url)
-                image.addClass("card-img-top");
-                card.append(image);
-                $(".gif-container").append(card);
-            }
-        });
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET"
+        // }).then(function(response){
+        //     console.log(response);
+        //     var results = response.data;
+        //     for (var i = 0; i < results.length; i++){
+        //         var card = $("<div>");
+        //         card.addClass("card");
+        //         card.css("width","18rem");
+        //         var image = $("<img>");
+        //         image.attr("src",results[i].images.fixed_height.url)
+        //         image.addClass("card-img-top");
+        //         card.append(image);
+        //         $(".gif-container").append(card);
+        //     }
+        // });
     };
 
     // function to make buttons from array and append to html
     function renderButtons() {
         for (var i = 0; i < options.length; i++) {
             var btn = $("<button>");
-            btn.addClass("actor");
+            btn.addClass("actor btn btn-success");
             btn.attr("data-name", options[i]);
             var actorBtn = btn.text(options[i]);
             $(".btn-container").append(actorBtn);
@@ -54,12 +54,35 @@ $(document).ready(function(){
         };
     });
 
-    // AJAX call function
-
     // function on click 
 
     $(document).on("click", ".actor", function(){
-        renderGifs();
+        // renderGifs();
+        var actor = $(this).attr("data-name");
+        console.log($(this).attr("data-name"))
+        // var queryURL = "https://api.giphy.com/v1/gifs/search?q" + actor + "&api_key=08uHsmK4UzYd8KoIXuWdAWPu4gZtNIB5&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        actor + "&api_key=08uHsmK4UzYd8KoIXuWdAWPu4gZtNIB5&limit=10";
+
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+            var results = response.data;
+            for (var i = 0; i < results.length; i++){
+                var card = $("<div>");
+                card.addClass("card");
+                card.css("width","16rem");
+                var image = $("<img>");
+                image.attr("src",results[i].images.fixed_height.url)
+                image.addClass("card-img-top");
+                card.append(image);
+                $(".gif-container").prepend(card);
+            }
+        });
+
     });
 
     renderButtons();
