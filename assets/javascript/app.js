25387ae4b07fd0c6958333fded1b5166b3b9bf29
Blake Neely <blakeneely21@gmail.com>
2019-08-27 21:51:22 -0500

    added function to animate gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,39 @@
 $(document).ready(function(){
-    var options = ["ryan gosling", "emma stone", "michael j fox", "john stamos", "steve carrell"];
+    var options = ["ryan gosling", "emma stone", "michael j fox", "will smith", "steve carrell"];
     var userInput;
-    var actor;
 
-    // function to get gifs from giphy
-    function renderGifs(){
-        // var actor = $(this).attr("data-name");
-        // console.log(this)
-        // var queryURL = "https://api.giphy.com/v1/gifs/search?q" + actor + "&api_key=08uHsmK4UzYd8KoIXuWdAWPu4gZtNIB5&limit=10";
-
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // }).then(function(response){
-        //     console.log(response);
-        //     var results = response.data;
-        //     for (var i = 0; i < results.length; i++){
-        //         var card = $("<div>");
-        //         card.addClass("card");
-        //         card.css("width","18rem");
-        //         var image = $("<img>");
-        //         image.attr("src",results[i].images.fixed_height.url)
-        //         image.addClass("card-img-top");
-        //         card.append(image);
-        //         $(".gif-container").append(card);
-        //     }
-        // });
-    };
-
-    // function to make buttons from array and append to html
-    function renderButtons() {
-        for (var i = 0; i < options.length; i++) {
-            var btn = $("<button>");
-            btn.addClass("actor btn btn-success");
-            btn.attr("data-name", options[i]);
-            var actorBtn = btn.text(options[i]);
-            $(".btn-container").append(actorBtn);
-        }
-    };
-
-    // function on click to push new strings to array from input box
-    $(document).on("click", "#submit", function(event){
-        event.preventDefault();
-        userInput = $("#userInput").val().trim();
-        if (userInput == "") {
-            return false;
-        }
+    // Function to pause and play gifs
+    function animateGifs(){
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          }
         else {
-            options.push(userInput);
-            $(".btn-container").empty();
-            $("#userInput").val("");
-            renderButtons();    
-        };
-    });
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still")
+        }
+    };
 
-    // function on click 
-
-    $(document).on("click", ".actor", function(){
+    // Function to render gifs from ajax call to giphy api
+    function renderGifs() {
         var actor = $(this).attr("data-name");
-        console.log($(this).attr("data-name"))
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + actor + "&api_key=08uHsmK4UzYd8KoIXuWdAWPu4gZtNIB5&limit=10";
-        // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        // actor + "&api_key=08uHsmK4UzYd8KoIXuWdAWPu4gZtNIB5&limit=10";
 
-        
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
             var results = response.data;
-            for (var i = 0; i < results.length; i++){                       // Loop 
+            console.log(results);
+            for (var i = 0; i < results.length; i++){                       // Loop to make card for each returned gif from giphy
                 var card = $("<div>");                                      // Create a div element and assign to variable
                 card.addClass("card m-2 border-dark");                      // Add bootstrap classes to div
                 card.css("width","16rem");                                  // Add bootstrap style of 16rem width to div
                 var image = $("<img>");                                     // Create img element and assign to variable
-                image.attr("src",results[i].images.fixed_height.url)        // Attach url source to image element
+                image.attr("src",results[i].images.fixed_height_still.url)        // Attach url source to image element
+                image.attr("data-still",results[i].images.fixed_height_still.url);
+                image.attr("data-animate",results[i].images.fixed_height.url);
                 image.addClass("card-img-top");                             // Add bootstrap class to img
                 card.append(image);                                         // Append img to card div
                 var cardBody = $("<div>");                                  // Create div element and assign to variable
@@ -96,8 +55,35 @@ $(document).ready(function(){
                 $(".gif-container").prepend(card);                          // Append card to gif-container
             }
         });
-
-    });
-
-    renderButtons();                                                        // Load buttons on page
+    };
+    
+    // Function to push new strings to options array from input box
+    function pushToArray(){
+        event.preventDefault();
+        userInput = $("#userInput").val().trim();
+        if (userInput == "") {
+            return false;
+        }
+        else {
+            options.push(userInput);
+            $(".btn-container").empty();
+            $("#userInput").val("");
+            renderButtons();    
+        };
+    };
+    
+    // Function to make buttons from array and append to html
+    function renderButtons() {
+        for (var i = 0; i < options.length; i++) {
+            var btn = $("<button>");
+            btn.addClass("actor btn btn-success");
+            btn.attr("data-name", options[i]);
+            var actorBtn = btn.text(options[i]);
+            $(".btn-container").append(actorBtn);
+        }
+    };
+    $(document).on("click", ".card-img-top", animateGifs);                  // Event listener to animate gifs
+    $(document).on("click", "#submit", pushToArray);                        // Event listener to for input box
+    $(document).on("click", ".actor", renderGifs);                          // Event listener to load gifs on html
+    renderButtons();                                                        // Render buttons on html
 });
